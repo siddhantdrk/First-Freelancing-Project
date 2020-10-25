03ca4b3c4740901f47172ce0bdc91be621be1635
Siddhant Khobragade <siddhantdrk@gmail.com>
2020-10-25 10:44:38 +0530

    project folder updated

---
+++ Order_eckn2015/app/src/main/java/com/example/order_eckn2015/AfterOTP.java
@@ -0,0 +1,14 @@
+package com.example.order_eckn2015;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AfterOTP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_after_o_t_p);
+    }
+}
---
+++ Order_eckn2015/app/src/main/java/com/example/order_eckn2015/OtpActivity.java
@@ -0,0 +1,14 @@
+package com.example.order_eckn2015;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OtpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_after_o_t_p);
+    }
+}
